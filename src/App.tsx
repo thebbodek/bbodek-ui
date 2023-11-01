ec3531d453a57f5d129c9a2d9867517a36a53274
Ben <rqdjbr@icloud.com>
2023-11-01 13:38:04 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <>
+      <div style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            justifyItems: "center",
+            alignItems: "center"
+            }}
+      >
+          나는 뽀득 디자인 시스템 이다
+      </div>
+    </>
+  )
+}
+
+export default App
