9359d33a164784658ed3ca4b1550af4dcb965a0f
baegofda <molly.daleo3o@gmail.com>
2023-11-16 17:42:08 +0900

    (#26) Color Theme Setting

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type ThemeColors = 
+  "white" | "black" | "gray-08" |
+  "gray-07" | "gray-06" | "gray-05" |
+  "gray-04" | "gray-03" | "gray-02" |
+  "gray-01" | "gray-00" | "primary-06" |
+  "primary-05" | "primary-04" | "primary-03" |
+  "primary-02" | "primary-01" | "primary-00" |
+  "error" | "warning" | "success"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,37 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#fff",
+        black: "#101828",
+        "gray-08": "#1C273E",
+        "gray-07": "#333D52",
+        "gray-06": "#7C8497",
+        "gray-05": "#A9B2C7",
+        "gray-04": "#C6CEDE",
+        "gray-03": "#DBE1ED",
+        "gray-02": "#EAEEF6",
+        "gray-01": "#EFF2F9",
+        "gray-00": "#F8FAFF",
+        "primary-06": "#002F75",
+        "primary-05": "#003C96",
+        "primary-04": "#004DC0",
+        "primary-03": "#206FE4",
+        "primary-02": "#3D89FA",
+        "primary-01": "#9DC5FF",
+        "primary-00": "#DEEBFF",
+        "error": "#FF5260",
+        "warning": "#FFBB6A",
+        "success": "#79D4B4"
+      }
+    },
   },
+  safelist: [
+    { pattern: /bg-(white|black|gray|primary|error|warning|success)(-\d+)?/ },
+    { pattern: /text-(white|black|gray|primary|error|warning|success)(-\d+)?/ },
+    { pattern: /border-(white|black|gray|primary|error|warning|success)(-\d+)?/ },
+    { pattern: /placeholder-(white|black|gray|primary|error|warning|success)(-\d+)?/ },
+  ],
   plugins: [],
 }
-
