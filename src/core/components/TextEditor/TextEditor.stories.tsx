25b890efbbf31325028fee83c47bc0a9ade2f69b
baegofda <molly.daleo3o@gmail.com>
2023-12-09 17:28:54 +0900

    (#0) npm publish (0.0.94)

--- src/core/components/TextEditor/TextEditor.stories.tsx
+++ src/core/components/TextEditor/TextEditor.stories.tsx
@@ -14,6 +14,7 @@ const meta = {
     },
   },
 } satisfies Meta<typeof TextEditor>;
+
 export default meta;
 
 export const SmallTextEditor = () => {
--- src/core/components/TextEditor/index.tsx
+++ src/core/components/TextEditor/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactQuill from "react-quill";
+import React, { Suspense } from "react";
 import "react-quill/dist/quill.snow.css";
+const ReactQuill = React.lazy(() => import("react-quill"));
 
 interface TextEditorProps {
   value: string;
@@ -12,19 +12,21 @@ export default function TextEditor({
   onChange,
 }: TextEditorProps) {
   return (
-    <ReactQuill
-        theme = "snow"
-        value = {value}
-        onChange = {onChange}
-        className = {"w-full h-full"}
-        modules = {{
-          toolbar: [
-            [{ "header": [ 1, 2, false ] }],
-            [ "bold", "italic", "underline", "strike" ],
-            [ { "list": "ordered" }, { "list": "bullet" }, { "indent": "-1" }, { "indent": "+1" } ],
-            ["link"],
-          ],
-        }}
-      />
+    <Suspense fallback = {<></>}>
+      <ReactQuill
+      theme = "snow"
+      value = {value}
+      onChange = {onChange}
+      className = {"w-full h-full"}
+      modules = {{
+        toolbar: [
+          [{ "header": [ 1, 2, false ] }],
+          [ "bold", "italic", "underline", "strike" ],
+          [ { "list": "ordered" }, { "list": "bullet" }, { "indent": "-1" }, { "indent": "+1" } ],
+          ["link"],
+        ],
+      }}
+    />
+    </Suspense>
   );
 }
