2895589cf879085fa728c31ac5d80684306148b1
JinJu <jin_jooo@naver.com>
2024-04-16 13:58:49 +0900

    Label props, style 변경

--- src/core/components/Label/types/index.ts
+++ src/core/components/Label/types/index.ts
@@ -1,20 +1,14 @@
-import { ThemeTypography } from "@/types";
 import { HTMLAttributes } from "react";
 
-import { ROUNDED, SIZE, VARIANTS } from "../constants";
+import { SIZE, VARIANTS } from "../constants";
 
 export type VariantsType = typeof VARIANTS[keyof typeof VARIANTS];
 
 export type SizeType = typeof SIZE[keyof typeof SIZE];
 
-export type RoundedType = typeof ROUNDED[keyof typeof ROUNDED];
-
-export type ThemeType = Extract<ThemeTypography, "body-01-bold" | "body-02-bold">;
-
 export interface LabelProps extends HTMLAttributes<HTMLDivElement>{
   variants: VariantsType
-  height: SizeType
-  theme?: ThemeType
-  rounded: RoundedType
+  size: SizeType
   label: string
+  icon?: React.ReactNode
 }
