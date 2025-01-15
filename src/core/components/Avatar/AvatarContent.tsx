e9dd80b23dfe09931b87773ec6632df99c838e56
JinJu <jin_jooo@naver.com>
2025-01-15 20:18:55 +0900

    Avatar 오류 수정

---
+++ src/core/components/Avatar/AvatarContent.tsx
@@ -0,0 +1,55 @@
+import { PropsWithChildren } from 'react';
+
+import { AVATAR_IMAGE_SIZE } from '@/core/components/Avatar/constants';
+import { AvatarContentProps } from '@/core/components/Avatar/types';
+import { getFirstLetter } from '@/utilities/letter';
+import Icon from '@/core/components/Icon';
+import AvatarSkeleton from '@/core/components/Avatar/AvatarSkeleton';
+
+const AvatarContent = ({
+  alt,
+  src,
+  size,
+  rounded,
+  showAllLetter,
+  isLoading,
+  hasImageError,
+  setIsLoading,
+  setHasImageError,
+  children,
+}: PropsWithChildren<AvatarContentProps>) => {
+  const renderer = () => {
+    if (hasImageError && alt) return showAllLetter ? alt : getFirstLetter(alt);
+
+    if (!hasImageError && src) {
+      return (
+        <>
+          {isLoading && <AvatarSkeleton size={size} rounded={rounded} />}
+          <img
+            src={src}
+            alt={alt || ''}
+            className={'h-full w-full object-cover'}
+            width={AVATAR_IMAGE_SIZE[size]}
+            height={AVATAR_IMAGE_SIZE[size]}
+            onLoad={() => setIsLoading(false)}
+            onError={() => setHasImageError(true)}
+          />
+        </>
+      );
+    }
+
+    if (children) {
+      const isString = typeof children === 'string';
+
+      if (isString && !showAllLetter) return getFirstLetter(children);
+
+      return children;
+    }
+
+    return <Icon iconKey={'user'} weight={'fill'} />;
+  };
+
+  return <>{renderer()}</>;
+};
+
+export default AvatarContent;
--- src/core/components/Avatar/index.tsx
+++ src/core/components/Avatar/index.tsx
@@ -1,6 +1,5 @@
-import { MouseEvent, PropsWithChildren, useRef, useState } from 'react';
+import { MouseEvent, PropsWithChildren, useState } from 'react';
 import clsx from 'clsx';
-import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 import {
@@ -9,17 +8,15 @@ import {
 } from '@/core/components/Button/ButtonBase/constants';
 import { COLOR_THEME_STYLES } from '@/constants/theme';
 import {
-  AVATAR_IMAGE_SIZE,
   AVATAR_SIZE,
   AVATAR_SIZE_VARIANTS,
 } from '@/core/components/Avatar/constants';
 import { AvatarProps } from '@/core/components/Avatar/types';
-import { getFirstLetter } from '@/utilities/letter';
-import { ColorThemeType } from '@/types';
-import Icon from '@/core/components/Icon';
-import useInitEffect from '@/hooks/useInitEffect';
+import useInitEffect from '@/hooks/effects/useInitEffect';
 import getAvatarColorTheme from '@/core/components/Avatar/utils/getAvatarColorTheme';
 import Popover from '@/core/components/Popover/PopoverBase';
+import AvatarContent from '@/core/components/Avatar/AvatarContent';
+import AvatarSkeleton from '@/core/components/Avatar/AvatarSkeleton';
 
 const Avatar = ({
   src,
@@ -39,58 +36,15 @@ const Avatar = ({
   useRandomColorTheme = false,
   ...props
 }: PropsWithChildren<AvatarProps>) => {
-  const [hasImageError, setHasImageError] = useState(false);
+  const [isReady, setIsReady] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [avatarColorTheme, setAvatarColorTheme] = useState<ColorThemeType>();
-
-  const updateAvatarColorTheme = () => {
-    setAvatarColorTheme(
-      getAvatarColorTheme({ colorTheme, alt, useRandomColorTheme }),
-    );
-  };
-
-  useInitEffect(updateAvatarColorTheme);
-
-  const renderer = () => {
-    if (hasImageError && alt) return showAllLetter ? alt : getFirstLetter(alt);
-
-    if (!hasImageError && src) {
-      return (
-        <>
-          {isLoading && (
-            <Skeleton
-              width={AVATAR_IMAGE_SIZE[size]}
-              height={AVATAR_IMAGE_SIZE[size]}
-            />
-          )}
-          <img
-            src={src}
-            alt={alt || ''}
-            className={clsx(
-              'h-full w-full object-cover',
-              isLoading ? 'hidden' : 'block',
-            )}
-            width={AVATAR_IMAGE_SIZE[size]}
-            height={AVATAR_IMAGE_SIZE[size]}
-            onLoad={() => setIsLoading(false)}
-            onError={() => {
-              setHasImageError(true);
-            }}
-          />
-        </>
-      );
-    }
-
-    if (children) {
-      const isString = typeof children === 'string';
-
-      if (isString && !showAllLetter) return getFirstLetter(children);
-
-      return children;
-    }
-
-    return <Icon iconKey={'user'} weight={'fill'} />;
-  };
+  const [hasImageError, setHasImageError] = useState(false);
+  const useImage = !hasImageError && src;
+  const avatarColorTheme = getAvatarColorTheme({
+    colorTheme,
+    alt,
+    useRandomColorTheme,
+  });
 
   const handleClick = (e: MouseEvent<HTMLDivElement>) => {
     if (disabled) return;
@@ -98,23 +52,40 @@ const Avatar = ({
     onClick?.(e);
   };
 
+  useInitEffect(() => setIsReady(true));
+
+  if (!isReady) return <AvatarSkeleton size={size} rounded={rounded} />;
+
   return (
     <Popover
       trigger={
         <div
           className={clsx(
-            'relative flex items-center justify-center overflow-hidden uppercase hover:z-[1]',
+            'relative flex items-center overflow-hidden uppercase hover:z-[1]',
+            (!isLoading || !useImage) && 'justify-center',
             disabled && 'cursor-not-allowed opacity-50',
             onClick && !disabled && 'cursor-pointer',
             className,
-            avatarColorTheme && COLOR_THEME_STYLES[avatarColorTheme],
+            COLOR_THEME_STYLES[avatarColorTheme],
             AVATAR_SIZE[size],
             BUTTON_ROUNDED[rounded],
           )}
           onClick={handleClick}
           {...props}
         >
-          {renderer()}
+          <AvatarContent
+            alt={alt}
+            src={src}
+            size={size}
+            rounded={rounded}
+            showAllLetter={showAllLetter}
+            isLoading={isLoading}
+            setIsLoading={setIsLoading}
+            hasImageError={hasImageError}
+            setHasImageError={setHasImageError}
+          >
+            {children}
+          </AvatarContent>
         </div>
       }
       popover={popover}
